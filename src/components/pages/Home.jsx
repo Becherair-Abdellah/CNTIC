99997594e9afa81d6c447a6fb15d1beb2dda0b3c
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2023-10-15 16:29:23 +0100

    add footer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import OurGoals from "../homeComponenets/OurGoals";
 import Members from "../homeComponenets/Members";
 import Qoute from "../homeComponenets/qoute/Qoute";
+import Footer from "../homeComponenets/Footer/Footer";
 function Home() {
   return (
     <div className="  bg-bodyBg">
