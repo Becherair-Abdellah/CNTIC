d4d1f7386dda03106eb075f57df1144b4318ee3c
Abdellah <abdeallahdz2004@gmail.com>
2023-12-13 21:08:12 +0100

    restart now

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -1,10 +1,15 @@
 
-
+import Navbar from '../homeComponenets/Navbar'
 function Posts() {
   return (
-    <div>
-      Posts
+    <>
+    <Navbar/>
+    <div className='mt-16'>
+      <div className="bg-primaryColor py-[4rem] px-[1rem]">
+        <h1 className="text-3xl font-bold text-TextredColor mb-8 text-white">We Have A more events in Our CNTIC CLUB </h1>
+      </div>
     </div>
+    </>
   )
 }
 
