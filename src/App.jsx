8f83fb82b9d7278f4b91237c9b72c0fcdbbe41a4
Abdellah <abdeallahdz2004@gmail.com>
2023-12-13 23:01:11 +0100

    lists of waht i added : -dashboard folder it content :-components - dashbaord - events - Users Email signin signup ResetPassword

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import Home from "./components/pages/Home";
 import Events from "./components/pages/Events";
 import ContactUs from "./components/pages/ContactUs";
 import Articals from "./components/OurArticles/Articals";
+import Dashboard from "./dashboard/components/Dashboard"
+import Signin from "./dashboard/Signin";
 function App() {
   return (
     <Router>
       <Routes>
+        {/* this route for testing is just to test login page okey! */}
+      <Route exact path="/signin" element={<Signin/>}/>
+        {/* <Route exact path="/dashboard" element={<Dashboard/>}/> */}
         <Route exact path="/" element={<Home />} />
         <Route exact path="/Posts" element={<Events />} />
         <Route exact path="/contact" element={<ContactUs />} />
---
+++ src/dashboard/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      Dashbaord Here
+    </div>
+  )
+}
+
+export default Dashboard
