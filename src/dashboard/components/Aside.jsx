eb09df1201deb4ac1831d993371cf3799cfb997c
Abdellah <abdeallahdz2004@gmail.com>
2023-12-16 12:31:46 +0100

    completed LinkUser compoenent

--- src/dashboard/components/Aside.jsx
+++ src/dashboard/components/Aside.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector,useDispatch } from 'react-redux';
 import {hide} from "../redux/Reducers"
+import LinksUser from './LinkUser';
+import logo from "../../../public/logo.png"
 function Aside() {
   const aside = useRef('');
   const dispatch = useDispatch()
   const showAside = useSelector(({show_and_hide_aside:{aside}})=>aside);
-  const styles = `animated-bg fixed h-[100vh] md:left-0 ${showAside?`left-0`:`left-[-100%]`} w-[300px] z-[11] top-0 duration-300 bg-white shadow-lg shadow-gray-400`
+  const styles = `animated-bg px-4 fixed h-[100vh] md:left-0 ${showAside?`left-0`:`left-[-100%]`} w-[300px] z-[11] top-0 duration-300 bg-white shadow-lg shadow-gray-400`
 
   useEffect(()=>{
    
@@ -20,8 +22,17 @@ function Aside() {
   },[])
   return (
     <aside ref={aside} className={styles}>
-    aside bar
-    <h1>abdellah</h1>
+     {/* logo */}
+     <div className='flex items-center gap-2 py-5 cursor-pointer' >
+      <img className='w-[50px]' src={logo} alt="" />
+      <h1 className='text-xl font-bold text-primaryColor'>CNTIC DASHBOARD</h1>
+     </div>
+    {/* links  */}
+    <div>
+      <LinksUser/>
+    </div>
+    {/* footer  */}
+    <div className='text-gray-500 text-sm font-normal absolute -bottom-[-12px] left-[60%] w-full -translate-x-[50%]'>© 2023 <strong className=' text-primaryColor'>CNTIC</strong>,Inc. All rights reserved</div>
   </aside>
   )
 }
---
+++ src/dashboard/components/LinkUser.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import {FiUsers,FiSettings,FiLogOut} from 'react-icons/fi'
+import {BsBox,BsTelephone,BsCalendar2Event  } from 'react-icons/bs'
+import {BiMessageSquareDots } from "react-icons/bi";
+import {RiArticleLine } from "react-icons/ri";
+import {LuLayoutDashboard } from "react-icons/lu"
+import { motion } from 'framer-motion'
+function LinksUser() {
+  return (
+    <motion.ul 
+      transition={{
+        duration: 0.1,
+      }} className=' duration-300   bg-white sh w-full rounded-md  mt-4 '>
+      <li className='flex items-center gap-3 p-3 px-2 text-[17px] hover:bg-gray-100  cursor-pointer bg-gradient-to-r from-blue-500 to-blue-400 mb-3 rounded-md text-white font-semibold' ><LuLayoutDashboard size={21}/> Dashboard</li>
+      <li className='flex items-center gap-3 p-3 px-2 text-[15px] hover:bg-gray-100  cursor-pointer  mb-3 rounded-md text-gray-600' ><FiUsers size={21}/> Users</li>
+      <li className='flex items-center gap-3 p-3 px-2 text-[15px] hover:bg-gray-100 cursor-pointer  mb-3 rounded-md text-gray-600' ><BsCalendar2Event   size={21}/>Events</li>
+      <li className='flex items-center gap-3 p-3 px-2 text-[15px] hover:bg-gray-100  cursor-pointer  mb-3 rounded-md text-gray-600' ><RiArticleLine   size={21} />Articles</li>
+      <li className='flex items-center gap-3 p-3 px-2 text-[15px] hover:bg-gray-100  cursor-pointer  mb-3 rounded-md text-gray-600' ><BiMessageSquareDots size={21}/>Messages</li>
+    </motion.ul>
+  )
+}
+
+export default LinksUser
