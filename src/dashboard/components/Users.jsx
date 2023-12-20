bdebde9b0ff907683a933b032d3ca66e7592aec7
Abdellah <abdeallahdz2004@gmail.com>
2023-12-20 17:12:41 +0100

    completed and finished of Users Page Component

--- src/dashboard/components/Users.jsx
+++ src/dashboard/components/Users.jsx
@@ -6,7 +6,7 @@ import { FaUsers } from 'react-icons/fa'
 import { RiAdminFill } from 'react-icons/ri'
 function Users() {
   return (
-    <div className='mt-5 w-full border-b-2 border-blue-500 '>
+    <div className=' mt-5 w-full border-b-2 border-blue-500 relative '>
       {/* intro  */}
       <div className='flex flex-col md:flex-row md:justify-between items-center w-full gap-5'>
 
@@ -20,7 +20,8 @@ function Users() {
       </div>
       <p className='text-gray-500 text-md font-semibold mt-8 leading-6 border-l-[3px] border-primaryColor pl-3 md:pl-1'>Hi Admin This All Members in your Website <strong className='text-primaryColor'>CNTIC Club</strong> You can to check of users and you can to block or delete account </p>
       {/* users  */}
-      <div className='mt-7 overflow-hidden overflow-x-visible'>
+      <div className='users mt-7 overflow-hidden overflow-x-visible relative'>
+        <div className='overlay hidden w-full h-full absolute left-0 top-0 bg-transparent  z-[1]'></div>
         <ul className='text-gray-500 w-[850px] md:w-full'>
             <li className=' grid grid-cols-manyCustom  p-2 text-white rounded-md mb-2 bg-gradient-to-r from-blue-700 to-blue-500    font-semibold'>
                 <span>User</span>
